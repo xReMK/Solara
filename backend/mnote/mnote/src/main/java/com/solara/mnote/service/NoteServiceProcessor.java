dc1dc7b5d913dea3c44d5346b9396e2feb6634a6
xReMK <19bd1a0532.kmit@gmail.com>
2026-05-05 23:29:45 +0530

    adding kafka docker-compose

--- backend/mnote/mnote/src/main/java/com/solara/mnote/service/NoteServiceProcessor.java
+++ backend/mnote/mnote/src/main/java/com/solara/mnote/service/NoteServiceProcessor.java
@@ -42,4 +42,20 @@ public class NoteServiceProcessor {
             Thread.currentThread().interrupt();
         }
     }
+    /*
+
+    imp note :
+
+    - Connection Deadlock Risk: Since the transaction has already committed, the AFTER_COMMIT phase is technically "clean-up" time
+    If you try to perform more database operations in a synchronous listener during this phase, you might get errors because
+    the original database connection might have already been closed or returned to the pool
+
+    - Read-Only State: By the time AFTER_COMMIT runs, the transaction is over. Without a new @Transactional or a separate thread,
+    you cannot easily save new data (like the generated vectors) back to the database
+
+    so that means once you get the float[] we shouldn't call another function from here to update the database with this value
+    for this, maybe another background service can be started/or which keeps running for every 5 minutes & it processes all notes
+    from the database where the <vector> column is nil.
+
+     */
 }
